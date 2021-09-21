39087fcd39a7dcd9c6c9f0ec77c803c7831f870a
Anuj Patel <anujmaitri@gmail.com>
2021-09-21 14:06:21 -0400

    Finished Lab 1

--- app/src/main/java/com/example/noteme/home.java
+++ app/src/main/java/com/example/noteme/home.java
@@ -2,13 +2,31 @@ package com.example.noteme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        button = (Button) findViewById(R.id.button);
+
+
+
+
     }
+
+    public void newNote(View v){
+        Intent intent =  new Intent(home.this, new_note.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/noteme/new_note.java
+++ app/src/main/java/com/example/noteme/new_note.java
@@ -2,7 +2,9 @@ package com.example.noteme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class new_note extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class new_note extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_note);
+    }
+
+    public void newHome(View v){
+        Intent intent =  new Intent(new_note.this, home.class);
+        startActivity(intent);
     }
 }
