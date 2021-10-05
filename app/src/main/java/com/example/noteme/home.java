b7ccd369c392a51330e69cdab7495f9d10ae8d47
Mitul2000 <patelmitul5053@gmail.com>
2021-10-05 13:53:06 -0400

    database added

--- app/src/main/java/com/example/noteme/home.java
+++ app/src/main/java/com/example/noteme/home.java
@@ -3,21 +3,33 @@ package com.example.noteme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.icu.text.LocaleDisplayNames;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class home extends AppCompatActivity {
 
     Button button;
+    DatabaseHelper mDatabaseHelper;
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         button = (Button) findViewById(R.id.button);
+        mListView = (ListView) findViewById(R.id.listviewId);
+        mDatabaseHelper = new DatabaseHelper(this);
 
-
-
+        populateListView();
 
     }
 
@@ -25,7 +37,19 @@ public class home extends AppCompatActivity {
         Intent intent =  new Intent(home.this, new_note.class);
         startActivity(intent);
     }
+    private void populateListView() {
+        Log.d("ListDataActivity", "populateListView; displaying data..." );
+        //getting data and adding it to the list
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
 
+        while(data.moveToNext())
+        {
+            listData.add(data.getString(1));
+        }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listData);
+        mListView.setAdapter(adapter);
+    }
 
 
 
--- app/src/main/java/com/example/noteme/new_note.java
+++ app/src/main/java/com/example/noteme/new_note.java
@@ -4,18 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class new_note extends AppCompatActivity {
+
+    DatabaseHelper mDatabaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_note);
+//        mDatabaseHelper.showtest();
+        Log.d("CreatedTest","message");
     }
 
     public void newHome(View v){
+
         Intent intent =  new Intent(new_note.this, home.class);
         startActivity(intent);
+    }
+
+    public void AddNote(View v){
+        String message = ((EditText)findViewById(R.id.notemessage)).getText().toString();
+        String noteTitle = ((EditText)findViewById(R.id.noteid)).getText().toString();
+        Log.d("TEST",message);
+
+//        if(message.length()!= 0 || noteTitle.length() != 0){
+//            boolean insertData = mDatabaseHelper.addData(message);
+//            if(insertData){
+//                Toast.makeText(this,"Data saved successfully",Toast.LENGTH_LONG).show();
+//            }
+//        }
+
     }
 }
