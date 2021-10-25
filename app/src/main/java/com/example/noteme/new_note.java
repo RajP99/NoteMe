14509bca7a972194d62ea8a5a1cba1aa31569410
RajP99 <rajparikh90@gmail.com>
2021-10-25 17:50:26 -0400

    saving code because I'm reinstalling android studio

--- app/src/main/java/com/example/noteme/new_note.java
+++ app/src/main/java/com/example/noteme/new_note.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,12 +18,14 @@ public class new_note extends AppCompatActivity {
     DBHelper db;
     String noteTitle;
     String message;
+    String colour;
 
     public static ArrayList<new_note> noteArrayList = new ArrayList<>();
 
-    public new_note(String noteTitle, String message) {
+    public new_note(String noteTitle, String message, String colour) {
         this.noteTitle = noteTitle;
         this.message = message;
+        this.colour = colour;
     }
     public new_note() {
     }
@@ -45,10 +49,20 @@ public class new_note extends AppCompatActivity {
         this.message = message;
     }
 
+    public String getColour() {return colour;}
+
+    public void setColor(String colour) {this.colour = colour;}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_note);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.colours, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         db = new DBHelper(this);
     }
@@ -61,11 +75,12 @@ public class new_note extends AppCompatActivity {
     public void AddNote(View v) {
         noteTitle = ((EditText) findViewById(R.id.noteid)).getText().toString();
         message = ((EditText) findViewById(R.id.notemessage)).getText().toString();
+        colour = ((Spinner)findViewById(R.id.spinner)).getSelectedItem().toString();
 
         if (TextUtils.isEmpty(noteTitle)) {
             Toast.makeText(this, "Empty note title not allowed!", Toast.LENGTH_SHORT).show();
         } else {
-            Boolean checkInsert = db.insertData(noteTitle, message);
+            Boolean checkInsert = db.insertData(noteTitle, message, colour);
             if (checkInsert) {
                 Toast.makeText(this, "Your note has been saved!", Toast.LENGTH_SHORT).show();
                 newHome(v);
